16157dc1c5c6c4609d1c8168df4a20643d08ba4e
shaggy-axel <kornerus@fjfi.cvut.cz>
2021-03-12 19:01:52 +0300

    Initial commit

---
+++ core/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+
+from core.models import Document
+
+class DocumentForm(forms.ModelForm):
+    class Meta:
+        model = Document
+        fields = ('description', 'document', )
